9684476da42925af724664926d0d00fbf15da265
root <root@debians>
2016-10-07 22:51:08 +0200

    c

--- instalaciondocker.sh
+++ instalaciondocker.sh
@@ -5,3 +5,5 @@ apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E8
 echo "deb https://apt.dockerproject.org/repo debian-jessie main" >> /etc/apt/sources.list
 apt-get update
 apt-get -y install docker-engine
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
