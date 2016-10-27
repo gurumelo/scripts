f9b41f857fd95c023eaa48e3932ec6292d846b51
gurumelo <javibc@esdebian.org>
2016-10-27 23:50:31 +0200

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -3,6 +3,8 @@ apt-get install python-pip python-dev git -y
 pip install PyYAML jinja2 paramiko
 git clone https://github.com/ansible/ansible.git
 cd ansible
+git submodule update --init --recursive
 make install
 mkdir /etc/ansible
 cp examples/hosts /etc/ansible/
+cp examples/ansible.cfg /etc/ansible/
