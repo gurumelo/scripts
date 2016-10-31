2cdacc7db93c53db848af6655d46bf639c507542
root <root@debians>
2016-10-31 16:47:57 +0100

    v

---
+++ virtualbox.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install gdebi
+wget http://download.virtualbox.org/virtualbox/5.1.8/virtualbox-5.1_5.1.8-111374~Debian~jessie_amd64.deb
+gdebi virtualbox-5.1_5.1.8-111374~Debian~jessie_amd64.deb
+wget http://download.virtualbox.org/virtualbox/5.1.8/Oracle_VM_VirtualBox_Extension_Pack-5.1.8-111374.vbox-extpack
+#Archivo→Preferencias→Extensiones→Agregar nuevo paquete→Oracle_VM_VirtualBox_Extension_Pack-5.1.8-111374.vbox-extpack
